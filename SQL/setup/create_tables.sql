6a95db85a6fc62ccc3ff340473f67f65f7b2315c
Berk3459 <152713419+Berk3459@users.noreply.github.com>
2025-11-27 13:27:48 +0300

    Add sales table for bulk data insertion
    Created a sales table for bulk data insertion.

--- SQL/setup/create_tables.sql
+++ SQL/setup/create_tables.sql
@@ -1,1 +1,12 @@
 
+--Creating sales table to be able to bulk insert our data into sql
+
+CREATE TABLE sales(
+DocumentID smallint,
+Date date,
+SKU smallint,
+Price float,
+Discount float,
+Customer smallint,
+Quantity smallint
+)
